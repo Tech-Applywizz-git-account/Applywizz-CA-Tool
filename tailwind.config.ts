7fdd549f068c36c31db546b620bb9b8faccfbc6c
Tech-Applywizz-git-account <techteam@Applywizz.com>
2026-05-07 12:44:51 +0530

    Client based access by jagadish

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-// all in fixtures is set to tailwind v3 as interims solutions
+// all in fixtures is set to tailwind v3 as interims solution
 
 const config: Config = {
     darkMode: ["class"],
